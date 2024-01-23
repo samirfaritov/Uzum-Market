93a19ad8485c544c9f3d601a14152c8b3880d18c
samirfaritov <samirfaritov6@gmail.com>
2024-01-23 21:26:55 +0500

    navbar finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var mySwiper = new Swiper(".swiper-container", {
+//     // Optional parameters
+//     loop: true,
+//     autoplay: {
+//       delay: 3000, // Autoplay delay in milliseconds
+//     },
 
+//     // If you need pagination
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+// });
