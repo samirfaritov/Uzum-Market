ea4f2a1b5bf32862f8a47b2af85eeb3580f0ae8e
samirfaritov <samirfaritov6@gmail.com>
2024-01-30 18:36:43 +0500

    start flo api

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+import * as data from "./main"
+
+data.getTodo()
+data.interval
+data.skidka
+data.state
+data.getProducts()
+
+
+import * as flo from "./flo"
+
+flo.getTodo()
+flo.state
+flo.flo
+flo.interval
+flo.getProducts()
