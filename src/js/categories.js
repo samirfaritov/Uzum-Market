3604a3e38a5d4b188143f6558aed2451579a7d91
samirfaritov <samirfaritov6@gmail.com>
2024-02-20 15:35:53 +0500

    count start

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,39 +1,51 @@
-function totalCalc() {
-  const cartItem = document.querySelectorAll(".content");
-  const totalPrice = document.querySelector(".price2");
+  // function totalCalc() {
+  //   const cartItem = document.querySelectorAll(".content");
+  //   const totalPrice = document.querySelector(".price2");
 
-  let total = 0;
-  cartItem.forEach((item) => {
-    let count = item.querySelector(".count").innerText;
-    let price = item.querySelector(".price2").innerText;
-    let totalP = parseInt(count) * parseInt(price);
-    total += totalP;
-  });
-  totalPrice.innerHTML = total;
-}
+  //   let total = 0;
+  //   cartItem.forEach((item) => {
+  //     let count = item.querySelector(".count").innerText;
+  //     let price = item.querySelector(".price2").innerText;
+  //     let totalP = parseInt(count) * parseInt(price);
+  //     total += totalP;
+  //   });
+  //   totalPrice.innerHTML = total;
+  // }
 
-totalCalc();
+  // totalCalc();
+
+let minus = document.querySelector(".minus");
 
 
-window.addEventListener('click', (event) => {
-  event.preventDefault()
-
+window.addEventListener("click", (event) => {
+  event.preventDefault();
+  let price = document.querySelector('.price2')
+  
   if (event.target.dataset.action === "plus") {
     const counterWrapper = event.target.closest(".kol");
     const counter = counterWrapper.querySelector(".count");
     counter.innerHTML++;
+    minus.style.color = "#000";
+    price.innerHTML *= 2
     
-    totalCalc();
+    // totalCalc();
   }
   
   if (event.target.dataset.action === "minus") {
     const counterWrapper = event.target.closest(".kol");
     const counter = counterWrapper.querySelector(".count");
     counter.innerHTML--;
-    totalCalc();
+    price.innerHTML /= 2
     
+
+    // totalCalc();
     if (counter.innerHTML <= 1) {
       counter.innerHTML = "1";
+      minus.style.color = "#ccc";
+    }
+    
+    if (counter.innerHTML <= 0) {
+      price.innerHTML = price.innerHTML
     }
   }
-})
+});
